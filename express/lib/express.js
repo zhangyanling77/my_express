7ad04e5595a4d78e751ca6070bf9265b83cf89a4
zhangyanling77 <18783226594@163.com>
2019-09-27 09:27:22 +0800

    Update express.js

--- express/lib/express.js
+++ express/lib/express.js
@@ -1,12 +1,11 @@
-// 真正express文件夹
 // 创建应用
-console.log('my')
 
 const Application = require('./application')
 const Router = require('./router/index')
+
 function createApplication(){
     // 创建 应用 核心就是new Application
     return new Application
 }
-createApplication.Router = Router;
+createApplication.Router = Router; // 将Router构造函数挂载到app上，因为是构造函数，所以既可以new，也可以直接调用
 module.exports = createApplication
